0d50e5aba86e8036f7ce5f7c7e4b3f9f7a6f9eb1
anthony <anthony.cecconi.91@gmail.com>
2023-01-16 14:33:58 +0100

    abbozzato DB

---
+++ script/script.js
@@ -0,0 +1,516 @@
+const volley = [
+    teams= [
+        {
+            id:1,
+            name:"i Gommosi",
+            point: 0,
+            matchPoint:0
+        },
+        {
+            id:2,
+            name:"Yakuza",
+            point: 0,
+            matchPoint:0
+        },
+        {
+            id:3,
+            name:"Forbidden",
+            point: 0,
+            matchPoint:0
+        },
+        {
+            id:4,
+            name:"all you can beat",
+            point: 0,
+            matchPoint:0
+        },
+        {
+            id:5,
+            name:"Peaky Blinders",
+            point: 0,
+            matchPoint:0
+        },
+        {
+            id:6,
+            name:"Volley Clowns",
+            point: 0,
+            matchPoint:0
+        },
+        {
+            id:7,
+            name:"Bad Boys",
+            point: 0,
+            matchPoint:0
+        },
+        {
+            id:8,
+            name:"The Predators",
+            point: 0,
+            matchPoint:0
+        }
+    ],
+    turns =[
+        {
+            turn : 1,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 2,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 3,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 4,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 5,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 6,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 7,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 8,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 9,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 10,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 11,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 12,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 13,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        },
+        {
+            turn : 14,
+            matchs : [
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                },
+                {
+                    homeId : 1,
+                    transferId :5,
+                    homePoint : 0,
+                    trasferPoint : 0,
+                    ween :homeId>transferId ? homeId:transferId
+                }
+            ]
+        }
+    ]
+]
